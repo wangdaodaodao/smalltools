fe4e9636e357c493f15586ee5c267721fdf64851
wangdaodaodao <ahking0516>
2018-09-28 16:01:49 +0800

    iuuu

--- 下载歌曲.py
+++ 下载歌曲.py
@@ -58,10 +58,12 @@ class Encrypyed():
     def create_secret_key(self, size):
         return binascii.hexlify(os.urandom(size))[:16]
 
+
 class Song():
     """
     歌曲对象,用于储存歌曲的信息
     """
+
     def __init__(self, song_id, song_name, song_num, song_url=None):
         self.song_id = song_id
         self.song_name = song_name
@@ -73,17 +75,18 @@ class Crawler():
     """
     网易云爬取API
     """
-    def __init__(self, timeout=60, cookie_path = '.'):
+
+    def __init__(self, timeout=60, cookie_path='.'):
         self.headers = {
             'Accept': '*/*',
-			'Accept-Encoding': 'gzip,deflate,sdch',
-			'Accept-Language': 'zh-CN,zh;q=0.8,gl;q=0.6,zh-TW;q=0.4',
-			'Connection': 'keep-alive',
-			'Content-Type': 'application/x-www-form-urlencoded',
-			'Host': 'music.163.com',
-			'Referer': 'http://music.163.com/search/',
-			'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.132 Safari/537.36'
-		}
+            'Accept-Encoding': 'gzip,deflate,sdch',
+            'Accept-Language': 'zh-CN,zh;q=0.8,gl;q=0.6,zh-TW;q=0.4',
+            'Connection': 'keep-alive',
+            'Content-Type': 'application/x-www-form-urlencoded',
+            'Host': 'music.163.com',
+            'Referer': 'http://music.163.com/search/',
+            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.132 Safari/537.36'
+        }
 
         self.session = requests.Session()
 
@@ -98,25 +101,26 @@ class Crawler():
         post 请求
         """
         data = self.ep.encrypted_request(params)
-        resp = self.session.post(url, data, timeout=self.timeout)
+        print(url)
+        resp = self.session.post(url, data=data, timeout=self.timeout)
         result = resp.json()
         if result['code'] != 200:
             click.echo('post error')
         else:
             return result
-
+        print(result)
     def search(self, search_content, search_type, limit=9):
         """
         搜索api
         """
         url = 'http://music.163.com/weapi/cloudsearch/get/web?csrf_token='
         params = {
-            's':search_content,
-            'type':search_type,
-            'offset':0,
+            's': search_content,
+            'type': search_type,
+            'offset': 0,
             'sub': 'false',
             'limit': limit
-            }
+        }
         result = self.post_request(url, params)
         return result
 
@@ -125,68 +129,71 @@ class Crawler():
         根据 音乐名搜索
         """
         result = self.search(song_name, search_type=1, limit=limit)
-        if reslut['result']['songCount'] <= 0:
+        if result['result']['songCount'] <= 0:
             click.echo('歌曲不存在!')
         else:
-            songs = result['result']['songs']
+            song = result['result']['songs']
             if quiet:
-                song_id, song_name = song[0]['id'],song[0]['name']
-                song = Song(song_id=song_id, song_name = song_name, song_num=song_num)
+                song_id, song_name = song[0]['id'], song[0]['name']
+                song = Song(song_id=song_id, song_name=song_name,
+                            song_num=song_num)
                 return song
-    
-    def get_song_url (self, song_id, bit_rate=320000):
+
+    def get_song_url(self, song_id, bit_rate=320000):
         """
         通过id获取歌曲地址
         """
-        url = ''
+        url = 'http://music.163.com/weapi/song/enhance/player/url?csrf_token='
         csrf = ''
         params = {
-            'ids':[song_id],
-            'br':bit_rate,
+            'ids': [song_id],
+            'br': bit_rate,
             'csrf_token': csrf
         }
         result = self.post_request(url, params)
+        print(result)
+        song_url = result['data'][0]['url']
         if song_url is None:
             click.echo('歌曲不存在!')
         else:
             return song_url
 
-
-
-    def get_song_by_url(self, url, song_name,song_num, folder):
+    def get_song_by_url(self, url, song_name, song_num, folder):
         """
         下载歌曲到本地
         """
         if not os.path.exists(folder):
             os.mkdir(folder)
-        fpath = os.path.join(folder, '{}-{}.mp3'.format(str(song_num), song_name))
-        
-        #判断文件名中,是否有特殊字符
+        fpath = os.path.join(
+            folder, '{}-{}.mp3'.format(str(song_num), song_name))
+
+        # 判断文件名中,是否有特殊字符
         if sys.platform == 'win32' or sys.platform == 'cygwin':
             validname = re.sub(r'[<>:/\\|?*]', '', song_name)
             if validname != song_name:
-                fpath = os.path.join(floder, '{}-{}.mp3'.format(str(song_num), song_name))
-        
-        #储存文件的方法,要判断文件是否存在,在用click库下载
+                fpath = os.path.join(
+                    floder, '{}-{}.mp3'.format(str(song_num), song_name))
+
+        # 储存文件的方法,要判断文件是否存在,在用click库下载
         if not os.path.exists(fpath):
-            resp = self.download_session.get(url, timeout=self.timeout, stream=True)
+            resp = self.download_session.get(
+                url, timeout=self.timeout, stream=True)
             length = int(resp.headers.get('content-length'))
             label = '正在下载{},{}kb'.format(song_name, int(length/1024))
-            
+
             with click.progressbar(length=length, label=label) as progressbar:
                 with open(fpath, 'wb') as song_file:
                     for chunk in resp.iter_content(chunk_size=1024):
                         if chunk:
                             song_file.write(chunk)
-                            progressbar.update(1024) 
-
-
+                            progressbar.update(1024)
 
 
 class Netease():
     """
     网易云下载
     """
+
     def __init__(self, timeout, folder, quiet, coolie_paht):
         self.crawler = Crawler(timeout, cookie_path)
         self.folder = '.' if folder is None else folder
@@ -195,13 +202,41 @@ class Netease():
     def download_song_by_search(self, song_name, song_num):
         """
         """
-        try:
-            song = self.crawler.search_song(song_name, song_num, self.quiet)
-        except:
-            click.echo('下载出错了.')
+    
+        song = self.crawler.search_song(song_name, song_num, self.quiet)
+        
         if song != None:
-            self.download_song_by_id(song.song_id, song.song_name, song.song_num, self.folder)
+            self.download_song_by_id(
+                song.song_id, song.song_name, song.song_num, self.folder)
 
+    def download_song_by_id(self, song_id, song_name, song_num, folder='.'):
+        """
+        通过id下载歌曲
+        """
+        url = self.crawler.get_song_url(song_id)
+        song_name = song_name.replace('/', '')
+        song_name = song_name.replace('.', '')
+        print(url)
+        self.crawler.get_song_by_url(url, song_name, song_num, folder)
+
+
+
+
+
+if __name__ == '__main__':
+    timeout = 60
+    output = 'Music'
+    quiet = True
+    cookie_path = 'Cookie'
+    netease = Netease(timeout, output, quiet, cookie_path)
+    music_list = '1.txt'
+    if os.path.exists(music_list):
+        with open(music_list, 'r') as f:
+            music_list = list(map(lambda x : x.strip(), f.readlines()))
+            for song_num, song_name in enumerate(music_list):
+                netease.download_song_by_search(song_name, song_num+1)
+    else:
+        click.echo('列表不存在')
 
 
 
